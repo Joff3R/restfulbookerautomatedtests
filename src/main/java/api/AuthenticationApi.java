08f98567efaff30e08e717ef80502bbd20728ca1
Bartosz Kutkowski <bartosz.kutkowski@pansa.pl>
2021-09-24 12:44:05 +0200

    Initial commit

---
+++ src/main/java/api/AuthenticationApi.java
@@ -0,0 +1,14 @@
+package api;
+
+import static common.CommonConstants.EMPTY;
+
+public class AuthenticationApi {
+
+    private static String token = "";
+
+    private static String getToken(String username, String password) {
+        if (EMPTY.equals(token)){
+        }
+        return "";
+    }
+}
---
+++ src/main/java/common/CommonConstants.java
@@ -0,0 +1,12 @@
+package common;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.*;
+
+@NoArgsConstructor(access = PRIVATE)
+public class CommonConstants {
+
+    public static final String EMPTY = "";
+}
